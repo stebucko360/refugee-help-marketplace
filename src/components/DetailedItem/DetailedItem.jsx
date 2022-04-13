a0f00745de4c5ae7e64ce5d408bb702ae1134f96
Stephen Buckley <stephenbuckley@Stephens-MacBook-Pro.local>
2022-04-13 10:56:40 +0100

    itemDescription page created still needs styling

--- src/components/DetailedItem/DetailedItem.jsx
+++ src/components/DetailedItem/DetailedItem.jsx
@@ -1,6 +1,29 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 import { Card } from "../UI/Card";
+import "./DetailedItem.css";
+import { items } from "../../dummyData/items";
 
 export const DetailedItem = () => {
-  return <Card></Card>;
+  const { category_name, item_id } = useParams();
+
+  const individualItem = items.filter((item) => {
+    return item.itemId === parseInt(item_id);
+  });
+
+  return (
+    <Card>
+      <div className="itemContainer">
+        <h2>{individualItem[0].itemName}</h2>
+        <img
+          src={individualItem[0].image}
+          alt={individualItem[0].itemName}
+          className="itemPic"
+        ></img>
+        <p>{individualItem[0].description}</p>
+        <h3>{individualItem[0].location}</h3>
+        <h4>{individualItem[0].contact}</h4>
+      </div>
+    </Card>
+  );
 };
--- src/dummyData/items.js
+++ src/dummyData/items.js
@@ -1,90 +1,132 @@
 export const items = [
-    {
-        itemId: 1,
-        itemName: 'Food Package',
-        category: 'Food',
-        image: "https://sciencemeetsfood.org/wp-content/uploads/2018/10/packaging2-770x360.jpg",
-        location: 'London'
-    },
-    {
-        itemId: 2,
-        itemName: 'Dining Set',
-        category: 'Furniture',
-        image: "https://www.housingunits.co.uk/media/catalog/product/cache/8dc3298bba34b0846ef73bc138720a2d/8/0/8008be8bd7d85faaa8a7741b072eb209.jpg",
-        location: 'Manchester'
-        },
+  {
+    itemId: 1,
+    itemName: "Food Package",
+    category: "Food",
+    image:
+      "https://sciencemeetsfood.org/wp-content/uploads/2018/10/packaging2-770x360.jpg",
+    location: "London",
+    description:
+      "This is an example description for an item, include information about the product, availability, and any other crucial information. contact details will be shared below.",
+    contact: "07451489744",
+  },
+  {
+    itemId: 2,
+    itemName: "Dining Set",
+    category: "Furniture",
+    image:
+      "https://www.housingunits.co.uk/media/catalog/product/cache/8dc3298bba34b0846ef73bc138720a2d/8/0/8008be8bd7d85faaa8a7741b072eb209.jpg",
+    location: "Manchester",
+    description:
+      "This is an example description for an item, include information about the product, availability, and any other crucial information. contact details will be shared below.",
+    contact: "07451489744",
+  },
 
-    {
-        itemId: 3,
-        itemName: 'Male Shirt',
-        category: 'Clothing',
-        image: "https://m.media-amazon.com/images/I/51xj16W6aJL._AC_UL320_.jpg",
-        location: 'Manchester'
-        },
-    {
-        itemId: 4,
-        itemName: 'Board Game',
-        category: 'Toys',
-        image: "https://darkroom.contagious.com/1000/ac3555c7de036faa80d2a56e2c3e89ec:e734b280686e9bb5c5e14706a0e777d4/2-secondary-image-dice-roll-gameplay.jpg",
-        location: 'Liverpool'
-        },
+  {
+    itemId: 3,
+    itemName: "Male Shirt",
+    category: "Clothing",
+    image: "https://m.media-amazon.com/images/I/51xj16W6aJL._AC_UL320_.jpg",
+    location: "Manchester",
+    description:
+      "This is an example description for an item, include information about the product, availability, and any other crucial information. contact details will be shared below.",
+    contact: "07451489744",
+  },
+  {
+    itemId: 4,
+    itemName: "Board Game",
+    category: "Toys",
+    image:
+      "https://darkroom.contagious.com/1000/ac3555c7de036faa80d2a56e2c3e89ec:e734b280686e9bb5c5e14706a0e777d4/2-secondary-image-dice-roll-gameplay.jpg",
+    location: "Liverpool",
+    description:
+      "This is an example description for an item, include information about the product, availability, and any other crucial information. contact details will be shared below.",
+    contact: "07451489744",
+  },
 
-    {
-        itemId: 5,
-        itemName: 'Football',
-        category: 'Sporting Equipment',
-        image: "https://sc04.alicdn.com/kf/H7fac8cb1dd834548b79b17be6bf671aeC.jpg",
-        location: 'Liverpool'
-        },
-     {
-        itemId: 6,
-        itemName: 'Laptop',
-        category: 'Technology',
-        image: "https://m.media-amazon.com/images/I/61sKN2jfoCL._AC_SL1500_.jpg",
-        location: 'London'
-        },   
-        {
-        itemId: 7,
-        itemName: 'Food Package',
-        category: 'Food',
-        image: "https://sciencemeetsfood.org/wp-content/uploads/2018/10/packaging2-770x360.jpg",
-        location: 'London'
-    },
-    {
-        itemId: 8,
-        itemName: 'Dining Set',
-        category: 'Furniture',
-        image: "https://www.housingunits.co.uk/media/catalog/product/cache/8dc3298bba34b0846ef73bc138720a2d/8/0/8008be8bd7d85faaa8a7741b072eb209.jpg",
-        location: 'Manchester'
-        },
+  {
+    itemId: 5,
+    itemName: "Football",
+    category: "Sporting Equipment",
+    image: "https://sc04.alicdn.com/kf/H7fac8cb1dd834548b79b17be6bf671aeC.jpg",
+    location: "Liverpool",
+    description:
+      "This is an example description for an item, include information about the product, availability, and any other crucial information. contact details will be shared below.",
+    contact: "07451489744",
+  },
+  {
+    itemId: 6,
+    itemName: "Laptop",
+    category: "Technology",
+    image: "https://m.media-amazon.com/images/I/61sKN2jfoCL._AC_SL1500_.jpg",
+    location: "London",
+    description:
+      "This is an example description for an item, include information about the product, availability, and any other crucial information. contact details will be shared below.",
+    contact: "07451489744",
+  },
+  {
+    itemId: 7,
+    itemName: "Food Package",
+    category: "Food",
+    image:
+      "https://sciencemeetsfood.org/wp-content/uploads/2018/10/packaging2-770x360.jpg",
+    location: "London",
+    description:
+      "This is an example description for an item, include information about the product, availability, and any other crucial information. contact details will be shared below.",
+    contact: "07451489744",
+  },
+  {
+    itemId: 8,
+    itemName: "Dining Set",
+    category: "Furniture",
+    image:
+      "https://www.housingunits.co.uk/media/catalog/product/cache/8dc3298bba34b0846ef73bc138720a2d/8/0/8008be8bd7d85faaa8a7741b072eb209.jpg",
+    location: "Manchester",
+    description:
+      "This is an example description for an item, include information about the product, availability, and any other crucial information. contact details will be shared below.",
+    contact: "07451489744",
+  },
 
-    {
-        itemId: 9,
-        itemName: 'Male Shirt',
-        category: 'Clothing',
-        image: "https://m.media-amazon.com/images/I/51xj16W6aJL._AC_UL320_.jpg",
-        location: 'Manchester'
-        },
-    {
-        itemId: 10,
-        itemName: 'Board Game',
-        category: 'Toys',
-        image: "https://darkroom.contagious.com/1000/ac3555c7de036faa80d2a56e2c3e89ec:e734b280686e9bb5c5e14706a0e777d4/2-secondary-image-dice-roll-gameplay.jpg",
-        location: 'Liverpool'
-        },
+  {
+    itemId: 9,
+    itemName: "Male Shirt",
+    category: "Clothing",
+    image: "https://m.media-amazon.com/images/I/51xj16W6aJL._AC_UL320_.jpg",
+    location: "Manchester",
+    description:
+      "This is an example description for an item, include information about the product, availability, and any other crucial information. contact details will be shared below.",
+    contact: "07451489744",
+  },
+  {
+    itemId: 10,
+    itemName: "Board Game",
+    category: "Toys",
+    image:
+      "https://darkroom.contagious.com/1000/ac3555c7de036faa80d2a56e2c3e89ec:e734b280686e9bb5c5e14706a0e777d4/2-secondary-image-dice-roll-gameplay.jpg",
+    location: "Liverpool",
+    description:
+      "This is an example description for an item, include information about the product, availability, and any other crucial information. contact details will be shared below.",
+    contact: "07451489744",
+  },
 
-    {
-        itemId: 11,
-        itemName: 'Football',
-        category: 'Sporting Equipment',
-        image: "https://sc04.alicdn.com/kf/H7fac8cb1dd834548b79b17be6bf671aeC.jpg",
-        location: 'Liverpool'
-        },
-     {
-        itemId: 12,
-        itemName: 'Laptop',
-        category: 'Technology',
-        image: "https://m.media-amazon.com/images/I/61sKN2jfoCL._AC_SL1500_.jpg",
-        location: 'London'
-        },  
-]
+  {
+    itemId: 11,
+    itemName: "Football",
+    category: "Sporting Equipment",
+    image: "https://sc04.alicdn.com/kf/H7fac8cb1dd834548b79b17be6bf671aeC.jpg",
+    location: "Liverpool",
+    description:
+      "This is an example description for an item, include information about the product, availability, and any other crucial information. contact details will be shared below.",
+    contact: "07451489744",
+  },
+  {
+    itemId: 12,
+    itemName: "Laptop",
+    category: "Technology",
+    image: "https://m.media-amazon.com/images/I/61sKN2jfoCL._AC_SL1500_.jpg",
+    location: "London",
+    description:
+      "This is an example description for an item, include information about the product, availability, and any other crucial information. contact details will be shared below.",
+    contact: "07451489744",
+  },
+];
