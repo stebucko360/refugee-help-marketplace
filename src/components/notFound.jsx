842a63cf01d8d576c8b7642d45d753bdb1badfd2
Stephen Buckley <stephenbuckley@Stephens-MacBook-Pro.local>
2022-05-12 14:08:19 +0100

    stopped wally pushing stuff

---
+++ src/components/notFound.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { Card } from "../components/notFound";
+
+export const notFound = () => {
+  return (
+    <Card>
+      <h2>Page Not Found</h2>
+    </Card>
+  );
+};
