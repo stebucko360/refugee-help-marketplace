22450c3dfcf131bc9cace15096045d8fd0ef95c9
Aymene <a.nibouche@gmail.com>
2022-05-12 10:54:34 +0100

    Added Icons dummy data and Accommodation

--- src/dummyData/items.js
+++ src/dummyData/items.js
@@ -129,4 +129,198 @@ export const items = [
       "This is an example description for an item, include information about the product, availability, and any other crucial information. contact details will be shared below.",
     contact: "07451489744",
   },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  {
+    itemId: 13,
+    itemName: "Desktop",
+    category: "Technology",
+    image: "https://i.dell.com/is/image/DellContent/content/dam/global-asset-library/Products/Desktops/XPS/8950/xs8950_xsy_11023rf_bk.psd?fmt=pjpg&pscan=auto&scl=1&wid=3210&hei=3635&qlt=100,0&resMode=sharp2&size=3210,3635",
+    location: "London",
+    description:
+      "This is an example description for an item, include information about the product, availability, and any other crucial information. contact details will be shared below.",
+    contact: "07451489744",
+  }, 
+  {
+    itemId: 14,
+    itemName: "iPad",
+    category: "Technology",
+    image: "https://m.media-amazon.com/images/I/81raq99wqRS._AC_SL1500_.jpg",
+    location: "Birmingham",
+    description:
+      "This is an example description for an item, include information about the product, availability, and any other crucial information. contact details will be shared below.",
+    contact: "07451489744",
+  }, 
+  {
+    itemId: 15,
+    itemName: "Olga",
+    category: "People",
+    image: "https://d.newsweek.com/en/full/2019797/ukraine-woman.jpg",
+    location: "London",
+    description:
+      "This is an example description for an item, include information about the product, availability, and any other crucial information. contact details will be shared below.",
+    contact: "07451489744",
+  }, 
+  {
+    itemId: 16,
+    itemName: "Sofiya",
+    category: "People",
+    image: "https://godatinginukraine.com/wp-content/uploads/2019/05/how-to-start-dating-ukrainian-women.jpg",
+    location: "Manchester",
+    description:
+      "This is an example description for an item, include information about the product, availability, and any other crucial information. contact details will be shared below.",
+    contact: "07451489744",
+  }, 
+  {
+    itemId: 17,
+    itemName: "Stepan",
+    category: "People",
+    image: "https://news.bbcimg.co.uk/media/images/73394000/jpg/_73394541_ukraine_voxpop.jpg",
+    location: "Bristol",
+    description:
+      "This is an example description for an item, include information about the product, availability, and any other crucial information. contact details will be shared below.",
+    contact: "07451489744",
+  },
+  {
+    itemId: 18,
+    itemName: "Ostap",
+    category: "People",
+    image: "https://onlineforlove.com/wp-content/uploads/2020/05/revan.jpg",
+    location: "Brighton",
+    description:
+      "This is an example description for an item, include information about the product, availability, and any other crucial information. contact details will be shared below.",
+    contact: "07451489744",
+  }, 
+  {
+    itemId: 19,
+    itemName: "Apple iPhone 13",
+    category: "Technology",
+    image: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-13-pro-family-hero?wid=940&hei=1112&fmt=png-alpha&.v=1644969385433",
+    location: "Brighton",
+    description:
+      "This is an example description for an item, include information about the product, availability, and any other crucial information. contact details will be shared below.",
+    contact: "07865789089",
+  },
+  {
+    itemId: 20,
+    itemName: "Apple Magic Mouse",
+    category: "Technology",
+    image: "https://mac4school.co.uk/wp-content/uploads/2020/04/Magic-mouse-1.jpg",
+    location: "Brighton",
+    description:
+      "This is an example description for an item, include information about the product, availability, and any other crucial information. contact details will be shared below.",
+    contact: "07451489744",
+  },
+  {
+    itemId: 21,
+    itemName: "Job Search Meet-up",
+    category: "Events",
+    image: "https://media.istockphoto.com/photos/top-view-of-a-white-desktop-concept-job-search-picture-id1279104620?k=20&m=1279104620&s=612x612&w=0&h=Lit4OzCRPW6Z5Pq1L4b9ZjUJvx6McLJySTLnUjJsECE=",
+    location: "Brighton",
+    description:
+      "This is an example description for an item, include information about the product, availability, and any other crucial information. contact details will be shared below.",
+    contact: "07451489744",
+  },
+  {
+    itemId: 22,
+    itemName: "Refugee Park Meet",
+    category: "Events",
+    image: "https://images.lbc.co.uk/images/151671?crop=16_9&width=660&relax=1&signature=9FWqQv9p5cvkTcsaLbrFDc_iDiM=",
+    location: "Brighton",
+    description:
+      "This is an example description for an item, include information about the product, availability, and any other crucial information. contact details will be shared below.",
+    contact: "07451489744",
+  },
+  {
+    itemId: 23,
+    itemName: "Cargo Trousers",
+    category: "Clothing",
+    image: "https://nohowstyle.com/media/catalog/product/n/h/nhw_6072_82d8b83c-155f-4c61-83ce-07ceb6673801.jpg",
+    location: "Brighton",
+    description:
+      "This is an example description for an item, include information about the product, availability, and any other crucial information. contact details will be shared below.",
+    contact: "07451489744",
+  },
+  {
+    itemId: 24,
+    itemName: "Socks",
+    category: "Clothing",
+    image: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/17f774fb-a2ba-46cb-b6bf-7a798594f5ee/everyday-cushioned-training-ankle-socks-lZ03sD.png",
+    location: "Brighton",
+    description:
+      "This is an example description for an item, include information about the product, availability, and any other crucial information. contact details will be shared below.",
+    contact: "07451489744",
+  },
+  {
+    itemId: 25,
+    itemName: "Trainers",
+    category: "Clothing",
+    image: "https://img.ssensemedia.com/images/b_white,g_center,f_auto,q_auto:best/221245F122000_1/comme-des-garcons-black-nike-edition-premier-sneaker-heels.jpg",
+    location: "Brighton",
+    description:
+      "This is an example description for an item, include information about the product, availability, and any other crucial information. contact details will be shared below.",
+    contact: "07451489744",
+  },
+  {
+    itemId: 26,
+    itemName: "Crewneck Sweatshirt",
+    category: "Clothing",
+    image: "https://media.gq.com/photos/623350e70bc7b3583ffa7768/master/w_1280%2Cc_limit/GQ-spring-MR-PORTER-3-001.jpg",
+    location: "Brighton",
+    description:
+      "This is an example description for an item, include information about the product, availability, and any other crucial information. contact details will be shared below.",
+    contact: "07451489744",
+  },
+  {
+    itemId: 27,
+    itemName: "T-Shirt",
+    category: "Clothing",
+    image: "https://static.zajo.net/content/mediagallery/zajo_dcat/image/product/types/X/9088.png",
+    location: "Brighton",
+    description:
+      "This is an example description for an item, include information about the product, availability, and any other crucial information. contact details will be shared below.",
+    contact: "07451489744",
+  },
+  {
+    itemId: 28,
+    itemName: "Black Hoodie",
+    category: "Clothing",
+    image: "https://www.fjackets.com/uk/product_images/b/970/Black_Hooded_Sweatshirt__11809_zoom.jpg",
+    location: "Brighton",
+    description:
+      "This is an example description for an item, include information about the product, availability, and any other crucial information. contact details will be shared below.",
+    contact: "07451489744",
+  },
+  {
+    itemId: 29,
+    itemName: "Studio Flat (M1)",
+    category: "Accommodation",
+    image: "https://blog.studios2let.com/wp-content/uploads/2019/09/london-studio-flat-rent-005.jpg",
+    location: "Manchester",
+    description:
+      "This is an example description for an item, include information about the product, availability, and any other crucial information. contact details will be shared below.",
+    contact: "07451489744",
+  },
+  {
+    itemId: 30,
+    itemName: "Room Share (SW3)",
+    category: "Accommodation",
+    image: "https://images.theconversation.com/files/187770/original/file-20170927-24212-1rn6iil.png?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=900.0&fit=crop",
+    location: "London",
+    description:
+      "This is an example description for an item, include information about the product, availability, and any other crucial information. contact details will be shared below.",
+    contact: "07451489744",
+  },
 ];
